f13782e0dfa70db2b6ac787a48e600c637f9d52b
Your Name <you@example.com>
2022-10-10 13:46:15 +0700

    Build APP date: Mon 10 Oct 2022 01:45:56 PM +07

--- static/js/781.ad41a1f3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmyweb=self.webpackChunkmyweb||[]).push([[781],{8781:function(t,e,r){r.r(e),r.d(e,{default:function(){return c}});var n=r(7689),s=r(1087),l=r(9256),o=r(2684),u=r(184);function c(){var t=(0,n.UO)().key;return(0,u.jsx)(l.ts,{title:"Workflow test API",children:(0,u.jsx)(s.rU,{to:"/tools/tasks/projects/".concat(t,"/workflow/variable"),children:(0,u.jsx)(o.O,{value:"Bi\u1ebfn l\u01b0u tr\u1eef"})})})}}}]);
-//# sourceMappingURL=781.ad41a1f3.chunk.js.map
